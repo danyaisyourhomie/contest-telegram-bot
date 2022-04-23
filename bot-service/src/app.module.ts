57e7b249735a61d952f2700b348e1644820ed4e7
Danil Kladnitskiy <daniil.kla@fora-soft.com>
2022-04-23 14:04:00 +0300

    removed unuzed services

--- bot-service/src/app.module.ts
+++ bot-service/src/app.module.ts
@@ -1,11 +1,13 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './../../api/src/auth/auth.module';
+import { AuthService } from './../../api/src/auth/auth.service';
 import { MagazinesModule } from './magazines/magazines.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [MagazinesModule],
+  imports: [AuthModule, MagazinesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AuthService, AppService],
 })
 export class AppModule {}
